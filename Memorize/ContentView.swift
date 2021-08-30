3db8dbead83b057f78f87cd7a4a339faac870fa7
fcamilletti <franco.camilletti.09@gmail.com>
2021-08-30 08:07:17 -0300

    Creating both sides of cards and tap gestures

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -15,25 +15,31 @@ struct ContentView: View {
             CardView()
             CardView()
         }
-        .padding(.all)
-         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+        .padding(.all).foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
-    
 }
 
 struct CardView: View {
+    @State var faceUp = true
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                .fill()
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                .stroke(lineWidth: 3)
-            Text("🥵")
-                .font(.largeTitle)
+            let shape = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            if faceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("🥵").font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            faceUp = !faceUp
         }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
